2568e464e7c428558af226bc05e13459d58f2d4a
Tushar Dave <imtushardave@gmail.com>
2019-10-20 09:19:06 +0530

    Home Screen ready

---
+++ app/src/main/java/com/techknightsrtu/kidslearn/ProgBasicActivity.java
@@ -0,0 +1,14 @@
+package com.techknightsrtu.kidslearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProgBasicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prog_basic);
+    }
+}
